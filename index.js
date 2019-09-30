83a9955bbabf75f4327e6b477c245c4559bcf281
maudnals <maud.nalpas@gmail.com>
2019-09-30 15:13:53 +0200

    feat: query the chain for did doc storage location

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const sdk = require("@kiltprotocol/sdk-js");
-
+// importing full sdk???
 const express = require("express");
 const driver = express();
 
@@ -9,27 +9,75 @@ const driver = express();
 
 // see https://services.kilt.io/contacts/5GZPvZadd2GWEZcUPEw2eentLsTZFoXjYPoozxsYJqaf6c5u
 
+// server
 const PORT = 8080;
 const URI_DID = "/1.0/identifiers/:did";
-const STORAGE_URL = "https://services.kilt.io/contacts"; // TODO query the chain to get this
-const PREFIX = "did:kilt:";
+// const STORAGE_URL = "https://services.kilt.io/contacts"; // TODO query the chain to get this
+
+// node
+const NODE = "wss://full-nodes.kilt.io:9944";
+
+// did consts
+const URL_SCHEME_ID = "did";
+const KILT_METHOD_ID = "kilt";
+const PREFIX = `${URL_SCHEME_ID}:${KILT_METHOD_ID}:`;
+
+// todo document each function input and output
+// todo javascript vs typescript???
+// todo prep for errors
+// todo proper log management
+
+function getDidDocStorageLocation(address) {
+  return getDidViaChain(address).then(response => response.documentStore);
+}
+
+function getDidViaChain(address) {
+  // todo should we reconnect each time??? or keep a connection open????
+  // todo close connection
+  sdk.default.connect(NODE);
+  return sdk.Did.queryByAddress(address);
+}
+
+function getKiltIdFromDid(did) {
+  return did.substring(PREFIX.length);
+}
 
 driver.get(URI_DID, function(req, res) {
   const did = req.params.did;
-  const key = did.substring(PREFIX.length);
-  const url = `${STORAGE_URL}/${key}`;
+  const address = getKiltIdFromDid(did);
 
-  fetch(url)
-    .then(response => response.json())
-    // jsonResponse.did is the did document
-    .then(jsonResponse => {
-      const didDocumentAsJSON = JSON.stringify(jsonResponse.did);
-      res.send(didDocumentAsJSON);
+  getDidDocStorageLocation(address)
+    .then(storageLocation => {
+      console.log("RESP", storageLocation);
+      return storageLocation;
     })
-    .catch(reason => {
-      console.log(reason);
-      res.sendStatus(404);
+    .then(storageLocation => {
+      // todo https vs other... ???
+      fetch(`https:${storageLocation}`)
+        .then(response => response.json())
+        // jsonResponse.did is the did document
+        .then(jsonResponse => {
+          const didDocumentAsJSON = JSON.stringify(jsonResponse.did);
+          res.send(didDocumentAsJSON);
+        })
+        .catch(reason => {
+          console.log(reason);
+          res.sendStatus(404);
+        });
     });
+
+  // const url = `${STORAGE_URL}/${address}`;
+  // fetch(url)
+  //   .then(response => response.json())
+  //   // jsonResponse.did is the did document
+  //   .then(jsonResponse => {
+  //     const didDocumentAsJSON = JSON.stringify(jsonResponse.did);
+  //     res.send(didDocumentAsJSON);
+  //   })
+  //   .catch(reason => {
+  //     console.log(reason);
+  //     res.sendStatus(404);
+  //   });
 });
 
 driver.listen(PORT, function() {
