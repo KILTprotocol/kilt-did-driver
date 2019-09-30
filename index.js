a6ce80e56f1ece3642d438bc9b1ad316a93048a3
maudnals <maud.nalpas@gmail.com>
2019-09-30 16:51:26 +0200

    refactor: moved to synchronous-like style

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const sdk = require("@kiltprotocol/sdk-js");
 const express = require("express");
-const driver = express();
 
 // todo importing full sdk???
 // todo document each function input and output
@@ -8,13 +7,11 @@ const driver = express();
 // todo prep for errors: no did property, wrong did...
 // todo proper log management
 // proper setup: github hooks, linting
-
 // see https://services.kilt.io/contacts/5GZPvZadd2GWEZcUPEw2eentLsTZFoXjYPoozxsYJqaf6c5u
 
 // server
 const PORT = 8080;
 const URI_DID = "/1.0/identifiers/:did";
-// const STORAGE_URL = "https://services.kilt.io/contacts"; // TODO query the chain to get this
 
 // node
 const NODE = "wss://full-nodes.kilt.io:9944";
@@ -24,44 +21,53 @@ const URL_SCHEME_ID = "did";
 const KILT_METHOD_ID = "kilt";
 const PREFIX = `${URL_SCHEME_ID}:${KILT_METHOD_ID}:`;
 
-function getDidDocStorageLocation(address) {
-  return getDidViaChain(address).then(response => response.documentStore);
+async function getDidDocStorageLocation(address) {
+  const did = await getDidViaChain(address);
+  return did.documentStore;
 }
 
-function getDidViaChain(address) {
-  // todo should we reconnect each time??? or keep a connection open????
-  // todo close connection
+async function getDidViaChain(address) {
   sdk.default.connect(NODE);
-  return sdk.Did.queryByAddress(address);
+  // this return value is thenable, finally will be called at last
+  return sdk.Did.queryByAddress(address).finally(() =>
+    // close chain connection
+    sdk.BlockchainApiConnection.getCached().then(blockchain => {
+      blockchain.api.disconnect();
+    })
+  );
 }
 
 function getKiltIdFromDid(did) {
   return did.substring(PREFIX.length);
 }
 
-driver.get(URI_DID, function(req, res) {
+const driver = express();
+
+driver.get(URI_DID, async function(req, res) {
   const did = req.params.did;
   const address = getKiltIdFromDid(did);
+  const storageLocation = await getDidDocStorageLocation(address);
 
-  getDidDocStorageLocation(address)
-    .then(storageLocation => {
-      console.log("RESP", storageLocation);
-      return storageLocation;
+  fetch(`https:${storageLocation}`)
+    .then(response => response.json())
+    .then(jsonResponse => {
+      // jsonResponse.did is the did document
+      const didDocumentAsJSON = JSON.stringify(jsonResponse.did);
+      res.send(didDocumentAsJSON);
     })
-    .then(storageLocation => {
-      // todo https vs other... ???
-      fetch(`https:${storageLocation}`)
-        .then(response => response.json())
-        .then(jsonResponse => {
-          // jsonResponse.did is the did document
-          const didDocumentAsJSON = JSON.stringify(jsonResponse.did);
-          res.send(didDocumentAsJSON);
-        })
-        .catch(reason => {
-          console.log(reason);
-          res.sendStatus(404);
-        });
+    .catch(reason => {
+      console.log(reason);
+      res.sendStatus(404);
     });
+  // getDidDocStorageLocation(address)
+  //   .then(storageLocation => {
+  //     console.log("RESP", storageLocation);
+  //     return storageLocation;
+  //   })
+  //   .then(storageLocation => {
+  //     // todo https vs other... ???
+
+  //   });
 });
 
 driver.listen(PORT, function() {
