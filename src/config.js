5875d64d2ac7f107a256ee0c7da70540d49de7e1
Antonio Antonino <antonio@kilt.io>
2021-11-10 13:54:56 +0100

    chore: move port number to env file

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ require('../envConfig')
 // blockchain node, see options in .env files and npm scripts
 const BLOCKCHAIN_NODE = process.env.KILT_BLOCKCHAIN_NODE
 // server port
-const PORT = 8080
+const PORT = process.env.SERVICE_PORT
 
 module.exports = Object.freeze({
   BLOCKCHAIN_NODE,
