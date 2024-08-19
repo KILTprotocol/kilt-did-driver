5d12c28ca176863f4faaaafbb1d48465b9315bdc
Raphael Flechtner <raphael@kilt.io>
2024-08-19 11:11:44 +0200

    refactor: use resolveRepresentation's content negotiation

--- src/index.js
+++ src/index.js
@@ -25,125 +25,126 @@ async function start() {
 
   // URI_DID is imposed by the universal-resolver
   driver.get(URI_DID, async (req, res) => {
-    async function handleRequest(responseContentType) {
-      let response
-      // Catch-all for generic error 500
-      try {
-        console.log('--------------------')
-        console.info('\n→ Received headers:')
-        console.info(JSON.stringify(req.headers, null, 2))
-        const { did } = req.params
+    let response = null
+    // Catch-all for generic error 500
+    try {
+      console.log('--------------------')
+      console.info('\n→ Received headers:')
+      console.info(JSON.stringify(req.headers, null, 2))
 
-        // 1. resolve DID
-        const {
-          didDocumentMetadata,
-          didResolutionMetadata,
-          didDocumentStream
-        } = await DidResolver.resolveRepresentation(did, {
-          accept:
-            responseContentType === DID_RESOLUTION_RESPONSE_MIME
-              ? 'application/did+ld+json'
-              : responseContentType
+      // content negotiation; what do we return?
+      let resolveContentType
+      let returnDocumentOnly = true
+      if (req.accepts([DID_RESOLUTION_RESPONSE_MIME, 'application/ld+json'])) {
+        returnDocumentOnly = false
+        resolveContentType = 'application/did+ld+json'
+      } else if (req.accepts(['application/did+ld+json'])) {
+        resolveContentType = 'application/did+ld+json'
+      } else if (req.accepts(['application/did+json', 'application/json'])) {
+        resolveContentType = 'application/did+json'
+      } else if (req.accepts(['application/did+cbor', 'application/cbor'])) {
+        resolveContentType = 'application/did+cbor'
+      } else {
+        ;[resolveContentType] = req.accepts() // returns all accepted media types, ordered by preference
+      }
+
+      const { did } = req.params
+
+      // 1. resolve DID
+      const { didDocumentMetadata, didResolutionMetadata, didDocumentStream } =
+        await DidResolver.resolveRepresentation(did, {
+          accept: resolveContentType
         })
-        if (didDocumentStream) {
-          console.info(
-            `\n↑↓ Resolved DID resource as ${didResolutionMetadata.contentType}`
-          )
-          if (didResolutionMetadata.contentType?.includes('json')) {
-            console.info(decoder.decode(didDocumentStream))
-          } else {
-            console.info(`0x${Buffer.from(didDocumentStream).toString('hex')}`)
-          }
-        }
-        // 2. set HTTP response code
-        if (didResolutionMetadata.error) {
-          switch (didResolutionMetadata.error) {
-            case 'invalidDid':
-              res.status(400)
-              break
-            case 'notFound':
-              res.status(404)
-              break
-            case 'representationNotSupported':
-              res.status(406)
-              break
-            case 'methodNotSupported':
-              res.status(501)
-              break
-            case 'internalError':
-            default:
-              res.status(500)
-          }
-          if (didResolutionMetadata.error === 'notFound') {
-            console.info(`\n🔍 DID ${did} not found (on chain)`)
-          } else {
-            console.error('\n⚠️ Could not resolve DID with given error:')
-            console.error(
-              `${didResolutionMetadata.error}: ${didResolutionMetadata.errorMessage}`
-            )
-          }
-        } else if (didDocumentMetadata.deactivated) {
-          console.info(`\n❌ DID ${did} has been disabled`)
-          // sending a 410 according to https://w3c-ccg.github.io/did-resolution/#bindings-https
-          res.status(410)
-        } else {
-          // set 200 status code
-          res.status(200)
-        }
-
-        // create response body depending on MIME type
-        if (responseContentType === DID_RESOLUTION_RESPONSE_MIME) {
-          // case A: DID resolution result
-          response = {
-            '@context': [DID_RESOLUTION_RESPONSE_CONTEXT],
-            didDocument: didDocumentStream
-              ? JSON.parse(decoder.decode(didDocumentStream))
-              : null,
-            didDocumentMetadata,
-            didResolutionMetadata
-          }
-        } else {
-          // case B: DID document only
-          response = didDocumentStream ? Buffer.from(didDocumentStream) : null
-        }
-      } catch (error) {
-        console.error(
-          '\n🚨 Could not satisfy request because of the following error:'
-        )
-        console.error(`${error}`)
-        res.status(500)
-        response = {
-          didDocument: null,
-          didDocumentMetadata: {},
-          didResolutionMetadata: {
-            error: 'internalError',
-            errorMessage: String(error)
-          }
-        }
-      } finally {
+      if (didDocumentStream) {
         console.info(
-          `\n← Responding with content type ${responseContentType} and body:`
+          `\n↑↓ Resolved DID resource as ${didResolutionMetadata.contentType}`
         )
-        console.info(JSON.stringify(response))
-        res.contentType(responseContentType).send(response)
-        console.log('--------------------')
+        if (didResolutionMetadata.contentType?.includes('json')) {
+          console.info(decoder.decode(didDocumentStream))
+        } else {
+          console.info(`0x${Buffer.from(didDocumentStream).toString('hex')}`)
+        }
       }
+      // 2. set HTTP response code
+      if (didResolutionMetadata.error) {
+        switch (didResolutionMetadata.error) {
+          case 'invalidDid':
+            res.status(400)
+            break
+          case 'notFound':
+            res.status(404)
+            break
+          case 'representationNotSupported':
+            res.status(406)
+            break
+          case 'methodNotSupported':
+            res.status(501)
+            break
+          case 'internalError':
+          default:
+            res.status(500)
+        }
+        if (didResolutionMetadata.error === 'notFound') {
+          console.info(`\n🔍 DID ${did} not found (on chain)`)
+        } else {
+          console.error('\n⚠️ Could not resolve DID with given error:')
+          console.error(
+            `${didResolutionMetadata.error}: ${didResolutionMetadata.errorMessage}`
+          )
+        }
+      } else if (didDocumentMetadata.deactivated) {
+        console.info(`\n❌ DID ${did} has been disabled`)
+        // sending a 410 according to https://w3c-ccg.github.io/did-resolution/#bindings-https
+        res.status(410)
+      } else {
+        // set 200 status code
+        res.status(200)
+      }
+
+      // create response body depending on MIME type
+      if (!returnDocumentOnly) {
+        // case A: DID resolution result
+        response = {
+          '@context': [DID_RESOLUTION_RESPONSE_CONTEXT],
+          didDocument: didDocumentStream
+            ? JSON.parse(decoder.decode(didDocumentStream))
+            : null,
+          didDocumentMetadata,
+          didResolutionMetadata
+        }
+        res.contentType(DID_RESOLUTION_RESPONSE_MIME)
+      } else if (didDocumentStream) {
+        // case B: DID document only
+        response = Buffer.from(didDocumentStream)
+        res.contentType(didResolutionMetadata.contentType)
+      }
+    } catch (error) {
+      console.error(
+        '\n🚨 Could not satisfy request because of the following error:'
+      )
+      console.error(`${error}`)
+      res.status(500)
+      response = {
+        didDocument: null,
+        didDocumentMetadata: {},
+        didResolutionMetadata: {
+          error: 'internalError',
+          errorMessage: String(error)
+        }
+      }
+      res.contentType(DID_RESOLUTION_RESPONSE_MIME)
+    } finally {
+      console.info(
+        `\n← Responding with headers ${JSON.stringify(
+          res.getHeaders(),
+          null,
+          2
+        )} and body:`
+      )
+      console.info(JSON.stringify(response))
+      res.send(response)
+      console.log('--------------------')
     }
-    // content negotiation; what do we return?
-    res.format({
-      [DID_RESOLUTION_RESPONSE_MIME]: () =>
-        handleRequest(DID_RESOLUTION_RESPONSE_MIME),
-      'application/did+ld+json': () => handleRequest('application/did+ld+json'),
-      'application/did+json': () => handleRequest('application/did+json'),
-      'application/json': () => handleRequest('application/did+json'),
-      'application/did+cbor': () => handleRequest('application/did+cbor'),
-      'application/cbor': () => handleRequest('application/did+cbor'),
-      default: () => {
-        const message = `Not acceptable media type(s) ${req.headers.accept}`
-        console.error(`Error: ${message}`)
-        res.status(406).send(message)
-      }
-    })
   })
 
   const server = driver.listen(PORT, () => {
